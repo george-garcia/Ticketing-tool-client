7297a943de009d6c20ee6a826898a1b672ac0b02
george-garcia <jorge.a.garcia12@gmail.com>
2023-01-02 00:31:20 -0600

    auth, routing, login pages
    login and register apis are done. currently working on the login and register pages. light routing has been added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import NavPage from "./pages/NavPage";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import {useFetchUsersQuery, useFetchOneUserQuery} from "./store";
 import {useRegisterUserQuery} from "./store";
 import { createApi } from '@reduxjs/toolkit/query/react'
 import {usersApi} from "./store/apis/usersApi";
 
 function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<LoginPage/>}/>
+                        <Route path="/register" element={<RegisterPage/>}/>
+                        {/*<Route path="/profile/:userId" element={<ProfilePage/>}/>*/}
+                    </Routes>
+            </BrowserRouter>
+            {/*<NavPage/>*/}
+
+        </div>
+    );
+}
 
     // const newUser = {
     //     firstName: "react",
@@ -12,13 +30,21 @@ function App() {
     //     email: "react@gmail.com",
     //     password: "secret"
     // };
-    const {data, error, isLoading} = useFetchUsersQuery();
-    // const {data, error, isLoading} = useRegisterUserQuery(JSON.stringify(newUser));
+
+    // console.log(newUser, person);
+    // console.log(newUser);
+    // const {data, error, isLoading} = useFetchUsersQuery();
+    // const data = useRegisterUserQuery(newUser);
     // const info = useFetchOneUserQuery('63aed8993751e5b44ae8ada2');
 
-    // if(data)
-    //     localStorage.set('token', 'Bearer ' + data.token);
+    // if(error){
+    //     console.log(error);
+    // }
 
+    // if(data) {
+    //     localStorage.set('token', 'Bearer ' + data.token);
+    //     console.log('register user query success and token created');
+    // }
 
     // localStorage.setItem('token', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2IwZmMyZjE1OWZmNDI1M2M3NzA3MTkiLCJmaXJzdE5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjcyNTY3OTAyLCJleHAiOjE2NzUxNTk5MDJ9.zv4x13rUPxPf5u1ZSBYBvtUUE9EwKBeG20cMvDg7tNE');
 
@@ -27,31 +53,25 @@ function App() {
     // console.log(query);
     // console.log(info);
 
-    let content;
-    if (isLoading) {
-        content = <div>Loading...</div>
-    } else if (error) {
-        content = <div>Error</div>
-    } else {
-        const {users} = data;
-        content = users.map(user => {
-            return (
-                <div>
-                    {user.firstName}
-                    {user.lastName}
-                    {user.email}
-                </div>
-            );
-        })
-    }
+    // let content;
+    // if (isLoading) {
+    //     content = <div>Loading...</div>
+    // } else if (error) {
+    //     content = <div>Error</div>
+    // } else {
+    //     const {users} = data;
+    //     content = users.map(user => {
+    //         return (
+    //             <div>
+    //                 {user.firstName}
+    //                 {user.lastName}
+    //                 {user.email}
+    //             </div>
+    //         );
+    //     })
+    // }
 
 
-    return (
-        <div>
-            App
-            {content}
-        </div>
-    );
-}
+
 
 export default App;
---
+++ src/pages/NavPage.js
@@ -0,0 +1,12 @@
+import './NavPage.css';
+const NavPage = () => {
+    return (
+        <div className="section-nav">
+            <div className="nav-top">top</div>
+            <div className="nav-left">left</div>
+            <div className="nav-body">body</div>
+        </div>
+    );
+}
+
+export default NavPage;
