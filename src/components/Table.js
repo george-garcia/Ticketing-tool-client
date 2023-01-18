ff1b2619f78b526e92254cfd52bbed585bd96e1a
george-garcia <jorge.a.garcia12@gmail.com>
2023-01-18 01:25:43 -0600

    updated pagination
    Pagination has been styled, now the last thing to fix is to implement the useEffect fix for when we are searching for pages.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,31 @@
 import {useNavigate} from "react-router-dom";
 import './Table.css';
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 
 
 function Table({data, config, keyFn, results}) {
     const toTicketPage = useNavigate();
     const [totalPages, setTotalPages] = useState(0);
     const [page, setPage] = useState(0);
+    let buttonRef = useRef();
+
 
     if (!results)
         results = 15;
 
     useEffect(() => {
         setTotalPages(Math.ceil(data.length / results));
-    })
+    });
+
+    // useEffect(() => {
+    //
+    //     if(!document.querySelector('.btn--active') && document.getElementById('0')) {
+    //         buttonRef = document.getElementById('0');
+    //         buttonRef.classList.add('.btn--active');
+    //         console.log(buttonRef);
+    //
+    //     }
+    // });
 
     function renderedData() {
         // if (data.length > results) {
@@ -31,27 +43,50 @@ function Table({data, config, keyFn, results}) {
         toTicketPage(`/tickets/${id}`);
     }
 
-    function handleSetPageOnClick(pageNumber) {
+    function handleSetPageOnClick(pageNumber, event) {
         setPage(pageNumber);
+
+        buttonRef = document.querySelector('.btn--active');
+        if (buttonRef)
+            buttonRef.classList.remove('btn--active');
+
+        event.target.classList.toggle('btn--active');
     }
 
     function nextButton(){
-        console.log(page, totalPages);
-        if((page + 1) < totalPages)
+        if((page + 1) < totalPages) {
             setPage(page => page + 1);
+
+            buttonRef = document.querySelector('.btn--active');
+            if (buttonRef)
+                buttonRef.classList.remove('btn--active');
+
+            buttonRef = document.getElementById(`${page + 1}`);
+            buttonRef.classList.add('btn--active');
+        }
     }
     function previousButton(){
-        console.log(page, totalPages);
-        if((page + 1) > 1)
+        if((page + 1) > 1) {
             setPage(page => page - 1);
+
+            buttonRef = document.querySelector('.btn--active');
+            if (buttonRef)
+                buttonRef.classList.remove('btn--active');
+
+            buttonRef = document.getElementById(`${page - 1}`);
+            buttonRef.classList.add('btn--active');
+            // console.log(buttonRef);
+
+
+        }
     }
 
     function renderNumberOfPageButtons() {
         // for every x results return a page
         const pageButtons = [];
 
-        for (let page = 0; page < totalPages; page++) {
-            pageButtons.push(<button key={page} className={"btn"} onClick={e => handleSetPageOnClick(page)}>{page + 1}</button>)
+        for (let page = 1; page < totalPages; page++) {
+            pageButtons.push(<button key={page} id={`${page}`} className={"btn--outline"} onClick={e => handleSetPageOnClick(page, e)}>{page + 1}</button>)
         }
         return pageButtons;
     }
@@ -87,9 +122,12 @@ function Table({data, config, keyFn, results}) {
             <div className={'table-footer'}>
                 <p>Showing {data.length < 1 ? 0 : 1 + (results * page)} to {renderedData().length + (results * page)} of {data.length} entries.</p>
                 <div className="table-footer--right">
-                    <button onClick={previousButton} className={"btn"}>Previous</button>
-                    <div className={"table-footer--buttons"}>{renderNumberOfPageButtons()}</div>
-                    <button onClick={nextButton} className={"btn"}>Next</button>
+                    <button onClick={previousButton} className={"btn--outline"}>Previous</button>
+                    <div className={"table-footer--buttons"}>
+                        <button id={0} className={"btn--outline btn--active"} onClick={e => handleSetPageOnClick(0, e)}>{1}</button>
+                        {renderNumberOfPageButtons()}
+                    </div>
+                    <button onClick={nextButton} className={"btn--outline"}>Next</button>
                 </div>
             </div>
         </>
