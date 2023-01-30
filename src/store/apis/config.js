f130771e4bd1350f814c62e2ed0d8c72354389d2
george-garcia <jorge.a.garcia12@gmail.com>
2023-01-29 19:22:32 -0600

    forgot the api server
    set the correct one this time

--- src/store/apis/config.js
+++ src/store/apis/config.js
@@ -1,7 +1,7 @@
 function config(){
 
     const LOCAL_SERVER= 'http://localhost:3001';
-    const PRODUCTION_SERVER='https://helpdeskhero.live';
+    const PRODUCTION_SERVER='https://helpdeskheroapis.live';
 
     return PRODUCTION_SERVER;
 }
