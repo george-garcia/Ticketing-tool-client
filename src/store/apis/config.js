fac4ef476596050f3eb47be616618fcfa6877871
george-garcia <jorge.a.garcia12@gmail.com>
2025-08-10 19:13:59 -0500

    fixed SSL
    I fixed the SSL on my vm so it should now correctly call the api at https instead of http

--- src/store/apis/config.js
+++ src/store/apis/config.js
@@ -1,6 +1,6 @@
 function config () {
   const LOCAL_SERVER = 'http://localhost:3001'
-  const PRODUCTION_SERVER = 'http://apis.georgegarciadev.com'
+  const PRODUCTION_SERVER = 'https://apis.georgegarciadev.com'
 
   return PRODUCTION_SERVER
 }
